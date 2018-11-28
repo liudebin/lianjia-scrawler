1dfc00339042a57416995cf1ab010efe3b090ec6
liuguobin <guobin.liu@holaverse.com>
2018-11-28 18:38:57 +0800

    pudong

--- scrawl.py
+++ scrawl.py
@@ -11,8 +11,8 @@ def get_communitylist():
 if __name__=="__main__":
     regionlist = settings.REGIONLIST # only pinyin support
     model.database_init()
-    core.GetHouseByRegionlist(regionlist)
+    # core.GetHouseByRegionlist(regionlist)
     core.GetRentByRegionlist(regionlist)
-    core.GetCommunityByRegionlist(regionlist) # Init,scrapy celllist and insert database; could run only 1st time
+    # core.GetCommunityByRegionlist(regionlist) # Init,scrapy celllist and insert database; could run only 1st time
     communitylist = get_communitylist() # Read celllist from database
     core.GetSellByCommunitylist(communitylist)
